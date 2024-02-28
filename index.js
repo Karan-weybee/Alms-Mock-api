7eefaeaf6d71ae33e5fa020d230784399f2f4b03
Abe Lincoln <mrabe@git.training>
2024-02-28 17:01:25 +0530

    details employee added

--- index.js
+++ index.js
@@ -11,4 +11,17 @@ app.get('/',(req,res)=>{
 app.get('/api/employees',(req,res)=>{
     return res.json(employees);
 })
+
+app.get('/api/employees/:id',(req,res)=>{
+
+    const id= Number(req.params.id);
+    const employee= employees.find((employee)=>employee.EmployeeId==id)
+    
+    const user = {...employee,
+                   "Projects":["xyz"],
+                    "Manger":"abc",
+                    "Date Of Birth":"7/18/2003"
+                }
+    return res.json(user);
+})
 app.listen(PORT,()=> console.log("server start ..."))
