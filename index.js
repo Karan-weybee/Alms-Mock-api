995c6777c4aebf3ae7a018480b3d0123c15fa0ab
Abe Lincoln <mrabe@git.training>
2024-02-28 16:08:14 +0530

    endpoint added

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const employees = require('./Employees.json')
-
+const endpoints = require('./EndPoints.json')
 const app = express();
 const PORT = 8000;
 //routes 
+app.get('/',(req,res)=>{
+    return res.json(endpoints);
+})
+
 app.get('/api/employees',(req,res)=>{
     return res.json(employees);
 })
